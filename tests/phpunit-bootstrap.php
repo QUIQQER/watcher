068463e8607971de3adf0de4940e1c173733101a
Henning <leutz@pcsg.de>
2026-08-20 20:49:58 +0200

    test: allow any configured CI database

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -11,29 +11,6 @@ if (!defined('QUIQQER_AJAX')) {
 require_once __DIR__ . '/DatabaseEnvironment.php';
 require_once __DIR__ . '/../../../../bootstrap.php';
 
-if (QUI\Watcher\Tests\DatabaseEnvironment::usesCiDatabase()) {
-    $databasePlatform = QUI::getDataBaseConnection()->getDatabasePlatform();
-    $databasePlatformClass = $databasePlatform::class;
-    $databaseVendor = QUI\Watcher\Tests\DatabaseEnvironment::getCiVendor();
-
-    if (!$databasePlatform instanceof Doctrine\DBAL\Platforms\AbstractMySQLPlatform) {
-        throw new RuntimeException(
-            'GitLab watcher tests expected a MySQL-compatible DBAL platform, got ' . $databasePlatformClass . '.'
-        );
-    }
-
-    $isMariaDbPlatform = str_contains(strtolower($databasePlatformClass), 'maria');
-
-    if (
-        ($databaseVendor === 'mariadb' && !$isMariaDbPlatform)
-        || ($databaseVendor === 'mysql' && $isMariaDbPlatform)
-    ) {
-        throw new RuntimeException(
-            'GitLab DB_VENDOR=' . $databaseVendor . ' does not match DBAL platform ' . $databasePlatformClass . '.'
-        );
-    }
-}
-
 spl_autoload_register(static function (string $class): void {
     if ($class === 'QUI\\Watcher') {
         require_once dirname(__DIR__) . '/src/QUI/Watcher.php';
